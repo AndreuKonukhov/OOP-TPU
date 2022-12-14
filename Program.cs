d4abddfa0166aa639ff4dfb718157d618c373f8f
Andreu Konukhov <akonuxov@list.ru>
2022-12-15 02:35:03 +0700

    Добавление функционала (Пользоваельский ввод)

--- Program.cs
+++ Program.cs
@@ -5,6 +5,8 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            string a = Console.ReadLine()
+            Console.WriteLine(a);
         }
     }
 }
